935b76d2b3d28e7d64ddf74d96e680985f663159
Juma Kanju <zekgyn@gmail.com>
2025-02-03 16:45:05 +0300

    test multi requests

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ payload = {"text": "Hello VFD!"}
 response = requests.post(url, json=payload)
 
 print(response.json())
+
+
+Invoke-RestMethod -Uri "http://127.0.0.1:5000/display" `
+  -Method Post `
+  -Headers @{"Content-Type"="application/json"} `
+  -Body '{"text": "2000/=TSH"}'
